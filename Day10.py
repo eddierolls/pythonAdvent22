db77e9f2a53a93a5e06244d2ca562cb3bae7a927
eddierolls <edwardjrolls@gmail.com>
2022-12-11 19:49:36 +0000

    Day 10 part B and Day 11 part A

--- Day10.py
+++ Day10.py
@@ -4,19 +4,31 @@ Created on Sun Dec 11 18:27:35 2022
 
 @author: edwar
 """
+def manageCycle(cycleNo,cycleCheck,xReg,score,picture):
+    cycleNo+=1
+    if cycleNo in cycleCheck:
+        score+=xReg*cycleNo
+    spritePos = (cycleNo-1)%40
+    if abs(xReg-spritePos)<=1:
+        picture.append('#')
+    else:
+        picture.append('.')
+    return cycleNo,score
+
+# Part A
 f = open('inputs/Day10.txt')
 cycleNo = 0
 xReg = 1
 score = 0
 cycleCheck = [20+40*x for x in range(6)]
+picture = []
 
 for line in f:
-    cycleNo+=1
-    if cycleNo in cycleCheck:
-        score+=xReg*cycleNo
+    cycleNo,score = manageCycle(cycleNo,cycleCheck,xReg,score,picture)
     if line.startswith("addx"):
-        cycleNo+=1
-        if cycleNo in cycleCheck:
-            score+=xReg*cycleNo
+        cycleNo,score = manageCycle(cycleNo,cycleCheck,xReg,score,picture)
         xReg+=int(line.strip().split(' ')[1])
 print(score)
+for i in range(6):
+    print(''.join(picture[i*40:(i+1)*40]))
+
---
+++ Day11.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 11 18:49:00 2022
+
+@author: edwar
+"""
+WORRYFACTOR = 3
+ROUNDS = 20
+
+def extractAfter(s,txt):
+    return s.strip().split(txt)[1]
+
+class Monkey(object):
+    def __init__(self,monkeyId):
+        self.inspections = 0
+        self.id=monkeyId
+    
+    def interpretStartingItems(self,line):
+        line = extractAfter(line,"Starting items: ")
+        self.items = list(map(int,line.split(", ")))
+        
+    def interpretOperation(self,line):
+        line = extractAfter(line,"Operation: new = old ")
+        if line=="* old":
+            self.operation = [0,0,1]
+        elif line[0]=="*":
+            self.operation = [0,int(line[1:]),0]
+        elif line[0]=="+":
+            self.operation = [int(line[1:]),1,0]
+        else:
+            ValueError("Line not recognised")
+    
+    def interpretTest(self,line):
+        line = extractAfter(line,"Test: divisible by ")
+        self.test = int(line)
+        
+    def interpretSendTo(self,line1,line2):
+        line1 = extractAfter(line1,"If true: throw to monkey ")
+        line2 = extractAfter(line2,"If false: throw to monkey ")
+        self.sendTo = [int(line2),int(line1)]
+
+# Initialize
+f = open('tests/Day11.txt')
+monkeyList = []
+monkeyId = 0
+while f.readline()!='': # EOF
+    thisMonkey = Monkey(monkeyId)
+    thisMonkey.interpretStartingItems(f.readline())
+    thisMonkey.interpretOperation(f.readline())
+    thisMonkey.interpretTest(f.readline())
+    thisMonkey.interpretSendTo(f.readline(),f.readline())
+    monkeyList.append(thisMonkey)
+    monkeyId+=1
+    f.readline()
+
+# Play
+for t in range(ROUNDS):
+    for monkey in monkeyList:
+        for item in monkey.items:
+            monkey.inspections+=1
+            item = sum([monkey.operation[i]*(item**i) for i in range(3)])
+            item = item//WORRYFACTOR
+            nextMonkey = monkey.sendTo[(item%monkey.test)==0]
+            monkeyList[nextMonkey].items.append(item)
+        monkey.items = []
+
+inspectionList = list(sorted([monkey.inspections for monkey in monkeyList],reverse=True))
+print(inspectionList[0]*inspectionList[1])
