795fec3aeb0e770514183442d8012647d541def3
eddierolls <edwardjrolls@gmail.com>
2022-12-15 21:54:56 +0000

    Part A but fast

--- Day15.py
+++ Day15.py
@@ -11,7 +11,10 @@ class Sensor(object):
     
     def findOnRow(self,row):
         distance = abs(row-self.centre[1])
-        points = set([(x+self.centre[0],row) for x in range(distance-self.radius,self.radius-distance+1)])
+        if self.radius-distance>=0:
+            points = (self.centre[0]-(self.radius-distance),self.centre[0]+(self.radius-distance))
+        else:
+            points = None
         return points
 
 f = open('inputs/Day15.txt')
@@ -19,6 +22,7 @@ ROW = 2000000
 overlap = set()
 sensors = []
 beacons = set()
+score = 0
 for line in f:
     line = line.strip().split("Sensor at x=")[1]
     centre = (int(line.split(',')[0]),int(line.split(', y=')[1].split(':')[0]))
@@ -27,10 +31,24 @@ for line in f:
     radius = abs(beacon[0]-centre[0]) + abs(beacon[1]-centre[1])
     sensors.append(Sensor(centre,radius))
     beacons.add(beacon)
-
-overlap = set()
+    
+ranges = []
 for sensor in sensors:
-    overlap = overlap.union(sensor.findOnRow(ROW))
-    print(sensor.centre)
+    newRange = sensor.findOnRow(ROW)
+    if newRange is not None:
+        ranges.append(newRange)
 
-print(len(overlap.difference(beacons)))
+rangeOut = []
+for r1 in ranges:
+    ix=0
+    while ix<len(rangeOut):
+        r2 = rangeOut[ix]
+        if r1[1]>=r2[0] and r1[0]<=r2[1]:
+            r1 = (min(r1[0],r2[0]),max(r1[1],r2[1]))
+            rangeOut.remove(r2)
+        else:
+            ix+=1
+    rangeOut.append(r1)
+
+score = sum([r[1]-r[0]+1 for r in rangeOut]) - sum([b[1]==ROW for b in beacons])
+print(score)
