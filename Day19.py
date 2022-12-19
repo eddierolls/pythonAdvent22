1732c96f78700bcaeaebe3bbfda88aa62f661ea2
eddierolls <edwardjrolls@gmail.com>
2022-12-19 21:50:58 +0000

    Definitely a contender for more optimisation... Over the recommended 15s by a couple of minutes, but I think more-of-the-same and/or more aggressive solution trimming would have gotten me there

--- Day19.py
+++ Day19.py
@@ -7,30 +7,30 @@ Created on Sun Dec 18 09:04:24 2022
 from copy import copy
 from dataclasses import dataclass
 
+PART2 = True
+
+if PART2:
+    MAXTIME = 32
+else:
+    MAXTIME = 24
+
 class Robot(object):
     def __init__(self,costs,resources):
         self.costs = costs # Ore Clay Obsidian
         self.resources = resources # Ore Clay Obsidian Points
 
-@dataclass(frozen=True)
-class State(object):
-    def __init__(self,robots,resources):
-        object.__setattr__(self,"robots",robots)
-        object.__setattr__(self,"resources",resources) # Ore Clay Obsidian Points
-    
-    def calcAfford(self,robots):
-        afford = [False]*4
-        for ixR in range(4):
-            if all([self.resources[x]>=robots[ixR].costs[x] for x in range(4)]):
-                afford[ixR] = True
-        return afford
-
-    def __eq__(self,other):
-        return self.robots==other.robots and self.resources==other.resources
+def calcAfford(state,robots):
+    afford = [False]*4
+    for ixR in range(4):
+        if all([state[1][x]>=robots[ixR].costs[x] for x in range(4)]):
+            afford[ixR] = True
+    return afford
 
 oreTypes = ["ore","clay","obsidian"]
 
-f = open("tests/Day19.txt")
+totalScore = []
+ixLine = 1
+f = open("inputs/Day19.txt")
 for line in f:
     line = line.strip().split('.')
     robots = []
@@ -46,24 +46,42 @@ for line in f:
         newRobot = Robot(tuple(costs),resources)
         robots.append(newRobot)
     
+    # Prune any states for more of resource X than ever needed
+    perMachine = [max([robot.costs[ixC] for robot in robots]) for ixC in range(3)]
+    perTime = {t:[perMachine[0]*(MAXTIME-1-t),perMachine[1]*(MAXTIME-1-t),perMachine[2]*(MAXTIME-1-t),99999999999] for t in range(MAXTIME)}
+
     # Breadth first search is easiest
-    nextStates = set([State((1,0,0,0),(0,0,0,0))])
-    for t in range(24):
+    nextStates = set([((1,0,0,0),(0,0,0,0))]) # A state is 4 robots then 4 resources. Order is ore, clay, obsidian, score
+    for t in range(MAXTIME):
         theseStates = nextStates
         nextStates = set()
+        maxIncr = (MAXTIME-t)*(MAXTIME-t+1)/2
+        topScore = max([state[1][3]+(state[0][3]*(MAXTIME-t-1)) for state in theseStates])
         for state in theseStates:
-            afford = state.calcAfford(robots)
+            if state[1][3]+state[0][3]*(MAXTIME-t-1)+maxIncr<=topScore:
+                continue
+            afford = calcAfford(state,robots)
             # Option 1 - do nothing - only if it can't afford all robots
-            if not all(afford):
-                newResource = tuple([state.resources[i]+state.robots[i] for i in range(4)])
-                newState = State(state.robots,newResource)
+            if not all(afford) or t==MAXTIME-1:
+                newResource = [state[1][i]+state[0][i] for i in range(4)]
+                newState = (state[0],tuple(newResource))
                 nextStates.add(newState)
             # Option 2 - loop through robots to buy
             for ixR in range(4):
-                if afford[ixR]:
-                    newResource = tuple([state.resources[i]+state.robots[i]-robots[ixR].costs[i] for i in range(4)])
-                    newRobots = list(state.robots)
+                if afford[ixR] and t!=MAXTIME-1 and state[1][ixR]+state[0][ixR]*(MAXTIME-t)<=perTime[t][ixR]:
+                    newResource = tuple([state[1][i]+state[0][i]-robots[ixR].costs[i] for i in range(4)])
+                    newRobots = list(state[0])
                     newRobots[ixR]+=1
-                    nextStates.add(State(tuple(newRobots),newResource))
-        topState = max([state.resources[2] for state in nextStates])
-        print(t,len(nextStates),topState)
+                    nextStates.add((tuple(newRobots),newResource))
+        print(ixLine,t,len(nextStates),topScore)
+    score = max([state[1][3] for state in nextStates])
+    totalScore.append(score)
+    ixLine+=1
+    if PART2 and ixLine>3:
+        break
+if PART2 and ixLine==3: # (tests)
+    print(totalScore[0]*totalScore[1])
+elif PART2: # (tests)
+    print(totalScore[0]*totalScore[1]*totalScore[2])
+else:
+    print(sum([totalScore[i]*(i+1) for i in range(len(totalScore))]))
